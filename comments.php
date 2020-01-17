56b09c427b3966f43cd66beee687066c9f95d185
Hoang Xuan Phuong <xuanphuong3105@gmail.com>
2020-01-17 11:16:00 +0700

    Switch to Bootstrap 4

--- comments.php
+++ comments.php
@@ -2,13 +2,14 @@
 /**
  * The template for displaying comments
  *
- * This is the template that displays the area of the page that contains both the current comments
+ * The area of the page that contains both current comments
  * and the comment form.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WeMap_Started_Theme
+ * @package understrap
  */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /*
  * If the current post is protected by a password and
@@ -20,56 +21,108 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div class="comments-area" id="comments">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
+	<?php // You can start editing here -- including this comment! ?>
+
+	<?php if ( have_comments() ) : ?>
+
 		<h2 class="comments-title">
+
 			<?php
-			$wemap_started_theme_comment_count = get_comments_number();
-			if ( '1' === $wemap_started_theme_comment_count ) {
+			$comments_number = get_comments_number();
+			if ( 1 === (int) $comments_number ) {
 				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'wemap-started-theme' ),
+					/* translators: %s: post title */
+					esc_html_x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'understrap' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $wemap_started_theme_comment_count, 'comments title', 'wemap-started-theme' ) ),
-					number_format_i18n( $wemap_started_theme_comment_count ),
+					/* translators: 1: number of comments, 2: post title */
+					esc_html( _nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'understrap'
+					) ),
+					number_format_i18n( $comments_number ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			}
 			?>
+
 		</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through. ?>
+
+			<nav class="comment-navigation" id="comment-nav-above">
+
+				<h1 class="sr-only"><?php esc_html_e( 'Comment navigation', 'understrap' ); ?></h1>
+
+				<?php if ( get_previous_comments_link() ) { ?>
+					<div class="nav-previous">
+						<?php previous_comments_link( __( '&larr; Older Comments', 'understrap' ) ); ?>
+					</div>
+				<?php } ?>
+
+				<?php	if ( get_next_comments_link() ) { ?>
+					<div class="nav-next">
+						<?php next_comments_link( __( 'Newer Comments &rarr;', 'understrap' ) ); ?>
+					</div>
+				<?php } ?>
+
+			</nav><!-- #comment-nav-above -->
+
+		<?php endif; // check for comment navigation. ?>
 
 		<ol class="comment-list">
+
 			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
+			wp_list_comments(
+				array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				)
+			);
 			?>
+
 		</ol><!-- .comment-list -->
 
-		<?php
-		the_comments_navigation();
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through. ?>
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wemap-started-theme' ); ?></p>
-			<?php
-		endif;
+			<nav class="comment-navigation" id="comment-nav-below">
 
-	endif; // Check for have_comments().
+				<h1 class="sr-only"><?php esc_html_e( 'Comment navigation', 'understrap' ); ?></h1>
 
-	comment_form();
-	?>
+				<?php if ( get_previous_comments_link() ) { ?>
+					<div class="nav-previous">
+						<?php previous_comments_link( __( '&larr; Older Comments', 'understrap' ) ); ?>
+					</div>
+				<?php } ?>
+
+				<?php	if ( get_next_comments_link() ) { ?>
+					<div class="nav-next">
+						<?php next_comments_link( __( 'Newer Comments &rarr;', 'understrap' ) ); ?>
+					</div>
+				<?php } ?>
+
+			</nav><!-- #comment-nav-below -->
+
+		<?php endif; // check for comment navigation. ?>
+
+	<?php endif; // endif have_comments(). ?>
+
+	<?php
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
+
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'understrap' ); ?></p>
+
+	<?php endif; ?>
+
+	<?php comment_form(); // Render comments form. ?>
 
 </div><!-- #comments -->
--- footer.php
+++ footer.php
@@ -2,35 +2,50 @@
 /**
  * The template for displaying the footer
  *
- * Contains the closing of the #content div and all content after.
+ * Contains the closing of the #content div and all content after
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WeMap_Started_Theme
+ * @package understrap
  */
 
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-	</div><!-- #content -->
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wemap-started-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wemap-started-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wemap-started-theme' ), 'wemap-started-theme', '<a href="#">PhuongHX</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+
+					<div class="site-info">
+
+						<?php understrap_site_info(); ?>
+
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
+
